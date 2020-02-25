f97017258e4a2b2e43586e5bdd94f2b03ede9c80
tommy <tommywhy1989@gmail.com>
2020-02-25 17:03:04 +1300

    week01

---
+++ 159731/example01/example01.cpp
@@ -0,0 +1,125 @@
+// System Headers
+#include <iostream>
+
+// GLFW Headers
+#include <GLFW/glfw3.h>
+
+
+// --------------------------------------------------------------------------------
+// GLFW Callbacks
+// --------------------------------------------------------------------------------
+
+// Called on Error Event
+void onError(int error, const char *description) {
+	// Print Error message
+	std::cerr << "Error: " << error << " : " << description << std::endl;
+}
+
+// Called on Window Close Event
+void onWindowClose(GLFWwindow *window) {
+	// Nothing to do right now
+	// Do not call glfwDestroyWindow from here
+}
+
+// Called on Window Size Event
+void onFramebufferSize(GLFWwindow *window, int width, int height) {
+	// Set-up the window/screen coordinates
+	glfwMakeContextCurrent(window);
+	glViewport(0, 0, width, height);
+	glfwMakeContextCurrent(NULL);
+}
+
+// --------------------------------------------------------------------------------
+// GLFW Functions
+// --------------------------------------------------------------------------------
+
+// Create a GLFW Window
+GLFWwindow* createWindow(int width, int height, const char *title, int major = 3, int minor = 2, GLFWmonitor *monitor = NULL, GLFWwindow *share = NULL) {
+	// Request an OpenGL context with specific features
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+
+	// If Version is 3 or higher
+	if (major >= 3) {
+		// Request Forward Compatibility
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		// If version is 3.2 or higher
+		if (major > 3 || minor >= 2) {
+			// Request Core Profile
+			glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		}
+	}
+
+	// Create GLFW Window
+	GLFWwindow *window = glfwCreateWindow(width, height, title, monitor, share);
+
+	// Check Returned Window
+	if (window == NULL) {
+		return NULL;
+	}
+
+	// Set Context
+	glfwMakeContextCurrent(window);
+
+	// Set window callback functions
+	glfwSetFramebufferSizeCallback(window, onFramebufferSize);
+	glfwSetWindowCloseCallback(window, onWindowClose);
+
+	// Return GLFW window
+	return window;
+}
+
+// --------------------------------------------------------------------------------
+// Example 01 - Simple Window
+// --------------------------------------------------------------------------------
+int main() {
+	// Set Error Callback
+	glfwSetErrorCallback(onError);
+
+	// Initialise GLFW
+	if (!glfwInit()) {
+		// Error - Abort
+		return 1;
+	}
+
+	// Create Window
+	GLFWwindow *window = createWindow(600, 600, "Example 01", 3, 2);
+
+	// Check Window
+	if (window == NULL) {
+		// Print Error Message
+		std::cerr << "Error: create window or context failed." << std::endl;
+
+		// Return Error
+		return 1;
+	}
+
+	// Main Render loop
+	while (!glfwWindowShouldClose(window)) {
+		// Make the context of the given window current on the calling thread
+		glfwMakeContextCurrent(window);
+
+		// Set clear (background) colour to green
+		glClearColor(0.f, 0.5f, 0.f, 0.f);
+
+		// Clear Screen
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap the back and front buffers
+		glfwSwapBuffers(window);
+
+		// Poll window events
+		glfwPollEvents();
+	}
+
+	// Stop receiving events for the window and free resources; this must be
+	// called from the main thread and should not be invoked from a callback
+	glfwDestroyWindow(window);
+
+	// Terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
+
+
---
+++ README.md
@@ -0,0 +1,78 @@
+# computer graphics 159.709
+#### Daniel Playne
+## week 01
+
+### course content
+1. real-time 3D rendering methods
+2. coordinate spaces & transforms
+3. 3D object representation
+
+* rendering
+> 2 dimentional image   
+> viewing frustrum(field of view)
+
+* 3D coordinates
+> vertexes  ->  edges ->    faces
+> polygonal modelling 
+
+* rasterisation **栅格化**
+>   the faces are (usually) filled in and drawn in the correct depth order.
+>  A polygon is ‘filled in’ by rasterisation, finding the pixels that it encom- passes.
+rasterisation => filled
+
+* shading    
+* texuring
+>  more realistic appearance
+ > Vertexes are mapped to a 2D texture by the use of** UV coordinates.**
+
+* projection
+ > perspective projection
+
+* rendering pipeline
+ > The graphics rendering pipeline (or just pipeline) has the job of taking the description of a scene(  *surfaces, materials, textures, lights, visual effects* ) and step-by-step turning it into the two-dimensional image that can be finally displayed on the screen.
+
+     1. application : 
+            run on the CPU  
+            updating the position
+            Detecting collisions between objects
+    
+     2. geometry
+         2.1   model & view tranform : model space; word space; eye space
+         2.2    vertex shading
+         2.3    projection
+                        orthographic正向影像     perspective透视投影
+         2.4    clipping 裁切
+         2.5    screen mapping
+                        screen coordinate
+
+     3. rasteriser
+         3.1 triangle setup
+         3.1 triangle traversal
+         3.1 pixel shading
+                fragement shader
+         3.1 merging
+                combination    stencil-buffer
+
+
+* z-buffering
+    color buffer     depth buffer
+
+* ray-tracing    光线追踪
+>especially important for reflections
+translucent surfaces
+computationally  expensive
+
+
+#### libs
+glfw : widow keyboard.. basic action
+glew: extension wrangle    使opengl 与 本机显卡 api matching
+> macos dont need glew
+
+
+
+
+
+
+
+
+
