8109a07463029d6e43ad2586022cfb51b0e390cb
tommy <tommywhy1989@gmail.com>
2020-03-17 19:48:02 +1300

    land

---
+++ assignment1/project01/cmake-build-debug/CMakeFiles/project01.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Tommy/Desktop/studyInMassey/159_709ComputerGraphics/assignment1/project01/src/main.cpp" "/Users/Tommy/Desktop/studyInMassey/159_709ComputerGraphics/assignment1/project01/cmake-build-debug/CMakeFiles/project01.dir/src/main.cpp.o"
+  "/Users/Tommy/Desktop/studyInMassey/159_709ComputerGraphics/assignment1/project01/src/shader.cpp" "/Users/Tommy/Desktop/studyInMassey/159_709ComputerGraphics/assignment1/project01/cmake-build-debug/CMakeFiles/project01.dir/src/shader.cpp.o"
+  "/Users/Tommy/Desktop/studyInMassey/159_709ComputerGraphics/assignment1/project01/src/utils.cpp" "/Users/Tommy/Desktop/studyInMassey/159_709ComputerGraphics/assignment1/project01/cmake-build-debug/CMakeFiles/project01.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  "PROJECT_SOURCE_DIR=\"/Users/Tommy/Desktop/studyInMassey/159_709ComputerGraphics/assignment1/project01\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/glfw/3.3/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ assignment1/project01/cmake-build-debug/CMakeFiles/project01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project01.dir/src/main.cpp.o"
+  "CMakeFiles/project01.dir/src/shader.cpp.o"
+  "CMakeFiles/project01.dir/src/utils.cpp.o"
+  "project01_Debug/project01.pdb"
+  "project01_Debug/project01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
