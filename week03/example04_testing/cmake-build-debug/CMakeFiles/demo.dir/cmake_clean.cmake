21479d069dc6d0587de165789ec7a0211bf19cb5
tommy <tommywhy1989@gmail.com>
2020-03-18 00:19:42 +1300

    project1: waving flag not working

---
+++ week03/example04_testing/cmake-build-debug/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/src/example04.cpp.o"
+  "CMakeFiles/demo.dir/src/shader.cpp.o"
+  "CMakeFiles/demo.dir/src/utils.cpp.o"
+  "demo_Debug/demo.pdb"
+  "demo_Debug/demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
