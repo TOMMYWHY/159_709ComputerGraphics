153a1677da4d79293ffb158f9ef23ae15ebfa790
tommy <tommywhy1989@gmail.com>
2020-04-17 15:19:33 +1200

    project02:need pause and shadow

---
+++ slef-backup/demo-self-9球+光+纹理/cmake-build-debug/CMakeFiles/opengl_demo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_demo.dir/src/Camera.cpp.o"
+  "CMakeFiles/opengl_demo.dir/src/LightPoint.cpp.o"
+  "CMakeFiles/opengl_demo.dir/src/geometry.cpp.o"
+  "CMakeFiles/opengl_demo.dir/src/image.cpp.o"
+  "CMakeFiles/opengl_demo.dir/src/main.cpp.o"
+  "CMakeFiles/opengl_demo.dir/src/shader.cpp.o"
+  "CMakeFiles/opengl_demo.dir/src/utils.cpp.o"
+  "opengl_demo_Debug/opengl_demo"
+  "opengl_demo_Debug/opengl_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
